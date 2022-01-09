90fdf448000d9b01b018f7cb45edbfee29edc765
dootrex <avigill@hotmail.ca>
2022-01-09 11:07:22 -0800

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2021 Avi, Adrian and Ross</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/globals/globals.js
+++
@@ -1,8 +0,0 @@
-// export const API_KEY = process.env.API_KEY;
-// export const API_TOKEN = process.env.API_TOKEN;
-// export const APP_FOLDER_NAME = process.env.APP_FOLDER_NAME;
-
-export const API_KEY = "467ae5ff4a2ad2a532b819a080fb0e52";
-export const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjdhZTVmZjRhMmFkMmE1MzJiODE5YTA4MGZiMGU1MiIsInN1YiI6IjVkZDQ0MDZjMjYzNDYyMDAxMmI2MTg4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EWiHqCznyW1scdahCqeDYIW0OvC25DgJab8GXnNOZTU";
-export const APP_FOLDER_NAME = "movie-mania";
