38a0d50ed64e9f9a3979f91cf9e16e97f6b4f0e3
dootrex <avigill@hotmail.ca>
2022-01-09 11:03:49 -0800

    add gh-pages

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,8 +1,20 @@
+import React from "react";
+import tmdb from '../images/tmdb.svg';
+
 function PageAbout() {
     return (
+        <>
         <section className="about-page">
-            <h2>About Page...</h2>
+            <h1>About Movie Mania</h1>
+            <p>Our mission is to provide users with a mass collection of movie data.</p>
+            <br></br>
+            <p>We believe that everyone deserves to gain access to precise information and reviews on that one movie they are interested in.</p>
+            <br></br>
+
+            <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+            <img src={tmdb} alt='tmbd logo' width='8%'/>
         </section>
+        </>
     )
 }
 
